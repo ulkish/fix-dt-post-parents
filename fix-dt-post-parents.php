4785daac63f055ea9cb4ba74214a65c28dd5f6a1
ulkish <dev.hugomoran@gmail.com>
2019-11-08 12:10:57 -0300

    Fixed plugin declaration.

--- fix-dt-post-parents.php
+++ fix-dt-post-parents.php
@@ -1,28 +1,30 @@
 <?php
 /**
-Plugin Name:       Fix Distributor Post Parents
-Plugin URI:        https://tipit.net/
-Description:       Fixes post parents connections on distributed posts.
-Version:           1.0
-Requires at least: 5.2
-Requires PHP:      7.2
-Author:            Hugo Moran
-Author URI:        https://tipit.net
-License:           GPL v2 or later
-License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Plugin Name:       Fix Distributor Post Parents
+ * Plugin URI:        https://tipit.net/
+ * Description:       Fixes post parents connections on distributed posts.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Hugo Moran
+ * Author URI:        https://tipit.net
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 
-Fix Distributor Post Parents is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-any later version.
+ * Fix Distributor Post Parents is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
 
-Fix Distributor Post Parents is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+ * Fix Distributor Post Parents is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with Fix Distributor Post Parents. If not, see https://www.gnu.org/licenses/gpl-2.0.html
+ * You should have received a copy of the GNU General Public License
+ * along with Fix Distributor Post Parents. If not, see https://www.gnu.org/licenses/gpl-2.0.html
 
-@package Fix Distributor Post Parents
+ * @package Fix Distributor Post Parents
  */
+
+
